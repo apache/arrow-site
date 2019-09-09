eabce20011cfb78af985ce5dbc63ecbfe5344697
Neal Richardson <neal.p.richardson@gmail.com>
2019-09-09 15:30:22 -0700

    Use ursa-labs/arrow-site to make a PR back to asf-site

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -ev
 
+UPSTREAM="apache/arrow-site"
+# GITHUB_TOKEN set in .travis.yml, encrypted
+# `hub` installed outside of this script, requires token called GITHUB_TOKEN with repo scope
+
 if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
-    if [ "${GITHUB_PAT}" = "" ] && [ "${DEPLOY_KEY}" = "" ]; then
-        # Don't build because we can't publish
+    if [ "${GITHUB_PAT}" = "" ] && [ "${DEPLOY_KEY}" = "" ] && [ "${TRAVIS_REPO_SLUG}" != "${UPSTREAM}" ]; then
+        # Don't build test site because we can't publish
         echo "To publish the site, you must set a GITHUB_PAT or DEPLOY_KEY at"
         echo "https://travis-ci.org/${TRAVIS_REPO_SLUG}/settings"
         exit 1
@@ -17,10 +21,13 @@ if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
     git config --global user.email "${AUTHOR_EMAIL}"
     git config --global user.name "${AUTHOR_NAME}"
 
-    if [ "${TRAVIS_REPO_SLUG}" = "apache/arrow-site" ]; then
+    if [ "${TRAVIS_REPO_SLUG}" = "${UPSTREAM}" ]; then
         # Production
         TARGET_BRANCH=asf-site
         BASE_URL=
+        GITHUB_PAT=$GITHUB_TOKEN
+        # We're going to push to this and make a PR back to $UPSTREAM
+        DESTINATION_REPO="ursa-labs/arrow-site"
     else
         # On a fork, so we'll deploy to GitHub Pages
         TARGET_BRANCH=gh-pages
@@ -29,6 +36,8 @@ if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
         if [ "${BASE_URL}" = "" ]; then
             BASE_URL=$(echo $TRAVIS_REPO_SLUG | sed -e 's@.*/@/@')
         fi
+        # We're going to push to a branch in the same repo
+        DESTINATION_REPO=$TRAVIS_REPO_SLUG
     fi
 
     # Build
@@ -40,10 +49,10 @@ if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
         eval $(ssh-agent -s)
         # Hack to make the key from the env var have real newlines
         echo "${DEPLOY_KEY}" | sed -e 's/\\n/\n/g' | ssh-add -
-        git clone -b ${TARGET_BRANCH} git@github.com:$TRAVIS_REPO_SLUG.git OUTPUT
+        git clone -b ${TARGET_BRANCH} git@github.com:$DESTINATION_REPO.git OUTPUT
     else
         echo "Using GitHub PAT"
-        git clone -b ${TARGET_BRANCH} https://${GITHUB_PAT}@github.com/$TRAVIS_REPO_SLUG.git OUTPUT
+        git clone -b ${TARGET_BRANCH} https://${GITHUB_PAT}@github.com/$DESTINATION_REPO.git OUTPUT
     fi
 
     rsync -a --delete --exclude '/.git/' --exclude '/docs/' build/ OUTPUT/
@@ -54,6 +63,10 @@ if [ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ]; 
         git add .
         git commit -m "Updating built site (build ${TRAVIS_BUILD_NUMBER})"
         git push origin ${TARGET_BRANCH}
+        if [ "${TRAVIS_REPO_SLUG}" = "${UPSTREAM}" ]; then
+            git remote add upstream https://github.com/${UPSTREAM}
+            hub pull-request -b upstream:asf-site --no-edit
+        fi
     else
         echo "No changes to the built site"
     fi
