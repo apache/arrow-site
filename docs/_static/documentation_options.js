fca661bde9ea2bbdcec68f43a0820eb9944d4aa2
Wes McKinney <wesm+git@apache.org>
2019-10-12 15:04:54 -0500

    Update docs for 0.15.0

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.14.2.dev0+g5f564424c.d20190805',
+    VERSION: '0.15.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
