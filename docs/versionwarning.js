0e43df73406b5869eaef0546f3052843e755e68c
Alenka Frim <frim.alenka@gmail.com>
2022-11-23 12:20:56 +0100

    Add versionwarning.js file and inserted script tag into all html files

---
+++ docs/versionwarning.js
@@ -0,0 +1,51 @@
+(function() {
+    // adapted 2022-11 from https://mne.tools/versionwarning.js
+    if (location.hostname == 'arrow.apache.org') {
+        var latestStable = '10.0.0';
+        var pre = '<div class="container-fluid alert-danger devbar"><div class="row no-gutters"><div class="col-12 text-center">';
+        var post = '</div></div></div>';
+        var anchor = 'class="btn btn-danger font-weight-bold ml-3 my-3 align-baseline"';
+        // Switch button message
+        var switch_dev = `Switch to unstable development release version`;
+        var switch_stable = `latest stable release (version ${latestStable})`;
+        // Links to stable or dev versions
+        var uri_dev_developers = "https://arrow.apache.org/docs/dev/developers/contributing.html";
+        var uri_dev = "https://arrow.apache.org/docs/dev/index.html";
+        var uri_stable = "https://arrow.apache.org/docs/index.html";
+
+
+        var versionPath = location.pathname.split('/')[2];
+        var subPath = location.pathname.split('/')[3];
+        if (versionPath == 'developers') {
+            // developers section in the stable version
+            var showWarning = `${pre}This is documentation for the stable version ` +
+                              `of Apache Arrow (version ${latestStable}). For latest development practices: ` +
+                              `<a ${anchor} href=${uri_dev_developers}>${switch_dev} </a>${post}`
+            $('.container-fluid').prepend(`${showWarning}`)
+        } else if (versionPath.match(/^\d/) < "4") {
+            // old versions 1.0,. 2.0 or 3.0
+            pre = '<p style="padding: 1em;font-size: 1em;border: 1px solid red;background: pink;">';
+            post = '</p>';
+            anchor = 'class="btn btn-danger" style="font-weight: bold; vertical-align: baseline;' +
+                     'margin: 0.5rem; border-style: solid; border-color: white;"';
+            var showWarning = `${pre}This is documentation for an old release of ` +
+                              `Apache Arrow (version ${versionPath}). Try the` +
+                              `<a ${anchor} href=${uri_stable}>${switch_stable}</a> or` +
+                              `<a ${anchor} href=${uri_dev}>development (unstable) version. </a>${post}`
+            $('.document').prepend(`${showWarning}`)
+        } else if (versionPath.match(/^\d/) && subPath == 'developers') {
+            // older versions of developers section (with numbered version in the URL)
+            var showWarning = `${pre}This is documentation for an old release of Apache Arrow ` +
+                              `(version ${versionPath}). For latest development practices: ` +
+                              `<a ${anchor} href=${uri_dev_developers}>${switch_dev} </a>${post}`
+            $('.container-fluid').prepend(`${showWarning}`)
+        } else if (versionPath.match(/^\d/)) {
+            // older versions (with numbered version in the URL)
+            var showWarning = `${pre}This is documentation for an old release of ` +
+                              `Apache Arrow (version ${versionPath}). Try the` +
+                              `<a ${anchor} href=${uri_stable}>${switch_stable}</a> or` +
+                              `<a ${anchor} href=${uri_dev}>development (unstable) version. </a>${post}`
+            $('.container-fluid').prepend(`${showWarning}`)
+        }
+    }
+})()
