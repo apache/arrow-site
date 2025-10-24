7d9d36e0ed0cf6f8334c56a22db5c7149fd71588
Raúl Cumplido <raulcumplido@gmail.com>
2025-10-24 12:47:24 +0200

    [Website] Update documentations for 22.0.0

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '21.0.0',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '22.0.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
